a5be459f1d9efce6685673bbc0c4df2d80811a89
Lucas Aguiar <lucas.9526@hotmail.com>
2023-01-27 14:57:40 -0300

    Criado o html

--- AppAngular/src/app/app.module.ts
+++ AppAngular/src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { PessoasService } from './pessoas.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from '@angular/forms';
+import { ModalModule } from "ngx-bootstrap/modal";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PessoasComponent } from './components/pessoas/pessoas.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PessoasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HttpClientModule, PessoasService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
